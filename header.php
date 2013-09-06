14ee5459c61b0fa8f84a06f60f75c09e5630b4f7
Thomas Kuntz <thomaskuntz67@gmail.com>
2013-09-06 19:43:41 +0200

    test

--- header.php
+++ header.php
@@ -28,8 +28,53 @@
 		<!--	<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 		-->
-
+<!--
+		<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+			<img class="smb-logo" width="1000" height="250" alt="Shared Memory Blog logo" src="<?php bloginfo('template_directory'); ?>/images/Bannière - V2.png"/>
+			<?php
+				$images = array();
+				if ( $img_dir = @opendir(get_theme_root() . "/" . get_template() . "/logo_icons") )
+				{
+					while ( false !== ($img_file = readdir($img_dir)) )
+					{
+						// checks for gif, jpg, png
+						if ( preg_match("/(\.gif|\.jpg|\.png)$/", $img_file) )
+						{
+							$images[] = $img_file;
+						}
+					}
+					closedir($img_dir);
+					
+					if(sizeof($images) > 0)
+					{
+						$rand = rand(0,sizeof($images) - 1);
+						$image_rand_1 = $images[$rand];
+						if(sizeof($images) > 1)
+						{
+							unset($images[$rand]);
+							$images=array_values($images);
+						}
+						
+						$image_rand_2 = $images[rand(0,sizeof($images) - 1)];
+						//~ echo "<img class=\"smb-logo-icon1\" width=\"270\" height=\"202\" alt=\"Shared Memory Blog icons 1\" src=\"" . get_bloginfo('template_directory') . "/logo_icons/$image_rand_1\"/>";
+						//~ echo "<img class=\"smb-logo-icon2\" width=\"270\" height=\"202\" alt=\"Shared Memory Blog icons 2\" src=\"" . get_bloginfo('template_directory') . "/logo_icons/$image_rand_2\"/>";
+					}
+				}
+			?>
+				<div class="smb-logo-icon1"> <?php if(isset($image_rand_1)) echo "<img alt=\"Shared Memory Blog icons 1\" src=\"" . get_bloginfo('template_directory') . "/logo_icons/$image_rand_1\"/>"; ?></div>
+				<div class="smb-logo-icon2"> <?php if(isset($image_rand_2)) echo "<img alt=\"Shared Memory Blog icons 2\" src=\"" . get_bloginfo('template_directory') . "/logo_icons/$image_rand_2\"/>"; ?></div>
+		</a>
+-->
 		
+		<?php
+			$screen_logo_array = array( array("screen 1_1.png", 250 , "logo_icons_1_1", 412 , 220 ), array("screen 4_3.png", 334 , "logo_icons_4_3", 305 , 222 ), array("screen 16_9.png", 443 , "logo_icons_16_9", 224 , 224 )) ;
+			$screen_rand_1 = rand(0,2);
+			$screen_rand_2 = rand(0,2);
+			//~ $screen_rand_2 = $screen_rand_1;
+			//~ $screen_rand_1 = 0;
+			//~ $screen_rand_2 = 0;
+			
+		?>
 		</div>
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
